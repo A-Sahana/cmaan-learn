790dc74200aeb934927a79ebcc1e9b639ce4bf9f
unknown <kishanangya22@gmail.com>
2024-10-29 11:21:07 +0530

    business path and page connection

---
+++ static/js/bussiness.js
@@ -0,0 +1,7 @@
+document.querySelector('.compare-plans').addEventListener('click', () => {
+    alert('Comparing plans...');
+});
+
+document.querySelector('.learn-more').addEventListener('click', () => {
+    alert('Learning more...');
+});
